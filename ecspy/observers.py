471fbf7e1b44548478a6a21d005da96c420a0666
aaron.lee.garrett <aaron.lee.garrett@ae8af3b4-1321-11de-99c7-9d618077940f>
2009-05-29 04:40:15 +0000

    Added docstrings to observers.py and terminators.py.

--- ecspy/observers.py
+++ ecspy/observers.py
@@ -1,19 +1,52 @@
 import time
 
 def default_observer(population, num_generations, num_fun_evals, args):
+    """Do nothing."""    
     pass
     
     
 def screen_observer(population, num_generations, num_fun_evals, args):
+    """Print the output of the EC to the screen.
+    
+    This function displays the results of the evolutionary computation
+    to the screen. The output includes the generation number, the current
+    number of function evaluations, the average fitness, the maximum
+    fitness, and the full population.
+    Arguments:
+    population -- the population of Individuals
+    num_generations -- the number of elapsed generations
+    num_fun_evals -- the number of of used function evaluations
+    args -- a dictionary of keyword arguments
+    
+    """
     print('Generation Number: %d' % num_generations)
     print('Function Evaluations: %d' % num_fun_evals)
     avg_fit = sum([x.fitness for x in population]) / float(len(population))
-    print('Average Fitness: %0.2f' % avg_fit)
+    print('Average Fitness: %0.5f     Maximum Fitness: %0.5f' % (avg_fit, population[0].fitness))
     for ind in population:
         print(str(ind))
     print('')
 
+    
 def file_observer(population, num_generations, num_fun_evals, args):
+    """Print the output of the EC to a file.
+    
+    This function saves the results of the evolutionary computation
+    to a file. The output includes the generation number, the current
+    number of function evaluations, the average fitness, the maximum
+    fitness, and the full population. The default action for the file
+    is to create a new file called 'ecspy_observer_file-<timestamp>'
+    in which to write the information.
+    Arguments:
+    population -- the population of Individuals
+    num_generations -- the number of elapsed generations
+    num_fun_evals -- the number of of used function evaluations
+    args -- a dictionary of keyword arguments
+
+    Optional keyword arguments in args:
+    observer_file -- a file object (default see text)
+    
+    """
     try:
         observer_file = args['observer_file']
     except KeyError:
@@ -24,7 +57,7 @@ def file_observer(population, num_generations, num_fun_evals, args):
     observer_file.write('Generation Number: %d \n' % num_generations)
     observer_file.write('Function Evaluations: %d \n' % num_fun_evals)
     avg_fit = sum([x.fitness for x in population]) / float(len(population))
-    observer_file.write('Average Fitness: %0.2f \n' % avg_fit)
+    observer_file.write('Average Fitness: %0.5f     Maximum Fitness: %0.5f' % (avg_fit, population[0].fitness))
     for ind in population:
         observer_file.write(str(ind) + '\n')
     observer_file.write('\n')
--- ecspy/terminators.py
+++ ecspy/terminators.py
@@ -9,12 +9,30 @@ def default_termination(population, num_generations, num_fun_evals, args):
     population -- the population of Individuals
     num_generations -- the number of elapsed generations
     num_fun_evals -- the number of of used function evaluations
+    args -- a dictionary of keyword arguments
     
     """
     return True
     
 
 def diversity_termination(population, num_generations, num_fun_evals, args):
+    """Return True if population diversity is less than a minimum diversity.
+    
+    This function calculates the Euclidean distance between every pair of
+    individuals in the population. It then compares the maximum of those
+    distances with a specified minimum required diversity. This terminator 
+    is really only well-defined for candidate solutions which are List 
+    types of numeric values. 
+    Arguments:
+    population -- the population of Individuals
+    num_generations -- the number of elapsed generations
+    num_fun_evals -- the number of of used function evaluations
+    args -- a dictionary of keyword arguments
+    
+    Optional keyword arguments in args:
+    min_diversity -- the minimum population diversity allowed (default 0.001)
+    
+    """
     try:
         min_diversity = args['min_diversity']
     except KeyError:
@@ -23,13 +41,29 @@ def diversity_termination(population, num_generations, num_fun_evals, args):
     distance = []
     for (p, q) in cart_prod:
         d = 0
-        for i in xrange(len(p.candidate)):
-            d += (p.candidate[i] - q.candidate[i])**2
+        for x, y in zip(p.candidate, q.candidate):
+            d += (x - y)**2
         distance.append(math.sqrt(d))
     return max(distance) < min_diversity
 
     
 def avg_fitness_termination(population, num_generations, num_fun_evals, args):
+    """Return True if the population's average fitness is near its maximum fitness.
+    
+    This function calculates the average fitness of the population as well
+    as the maximum (i.e., best) fitness. If the difference between those values
+    is less than a specified minimum, the function returns True. 
+    Arguments:
+    population -- the population of Individuals
+    num_generations -- the number of elapsed generations
+    num_fun_evals -- the number of of used function evaluations
+    args -- a dictionary of keyword arguments
+    
+    Optional keyword arguments in args:
+    min_fitness_diff -- the minimum allowable difference between 
+                        average and maximum fitness (default 0.001)
+    
+    """
     try:
         min_fitness_diff = args['min_fitness_diff']
     except KeyError:
@@ -40,6 +74,21 @@ def avg_fitness_termination(population, num_generations, num_fun_evals, args):
 
 
 def fun_eval_termination(population, num_generations, num_fun_evals, args):
+    """Return True if the number of function evaluations meets or exceeds a maximum.
+    
+    This function compares the number of function evaluations that have been 
+    generated with a specified maximum. It returns True if the maximum is met
+    or exceeded.
+    Arguments:
+    population -- the population of Individuals
+    num_generations -- the number of elapsed generations
+    num_fun_evals -- the number of of used function evaluations
+    args -- a dictionary of keyword arguments
+    
+    Optional keyword arguments in args:
+    max_fun_evals -- the maximum function evaluations (default len(population)) 
+    
+    """
     try:
         max_fun_evals = args['max_fun_evals']
     except KeyError:
@@ -48,6 +97,20 @@ def fun_eval_termination(population, num_generations, num_fun_evals, args):
 
 
 def num_gen_termination(population, num_generations, num_fun_evals, args):
+    """Return True if the number of generations meets or exceeds a maximum.
+    
+    This function compares the number of generations with a specified 
+    maximum. It returns True if the maximum is met or exceeded.
+    Arguments:
+    population -- the population of Individuals
+    num_generations -- the number of elapsed generations
+    num_fun_evals -- the number of of used function evaluations
+    args -- a dictionary of keyword arguments
+    
+    Optional keyword arguments in args:
+    max_generations -- the maximum generations (default 1) 
+    
+    """
     try:
         max_generations = args['max_generations']
     except KeyError:
