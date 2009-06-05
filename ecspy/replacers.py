b8201c82c95baf4eec4e14292e4799369b4b55f2
aaron.lee.garrett <aaron.lee.garrett@ae8af3b4-1321-11de-99c7-9d618077940f>
2009-06-05 14:32:02 +0000

    Added docstring to generational_replacement.

--- ecspy/replacers.py
+++ ecspy/replacers.py
@@ -20,6 +20,26 @@ def steady_state_replacement(random, population, parents, offspring, args):
 
 
 def generational_replacement(random, population, parents, offspring, args):
+    """Performs generational replacement with optional weak elitism.
+    
+    This function performs generational replacement, which means that
+    the entire existing population is replaced by the offspring,
+    truncating to the population size if the number of offspring is 
+    larger. Weak elitism may also be specified through the num_elites
+    keyword argument in args. If this is used, the best num_elites
+    individuals in the current population are allowed to survive if
+    they are better than the worst num_elites offspring.
+    Arguments:
+    random -- the random number generator object
+    population -- the population of Individuals
+    parents -- the list of parent individuals
+    offspring -- the list of offspring individuals
+    args -- a dictionary of keyword arguments
+
+    Optional keyword arguments in args:
+    num_elites -- number of elites to consider (default 0)
+    
+    """
     try:
         num_elites = args['num_elites']
     except KeyError:
