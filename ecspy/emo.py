2d5c0d97f713ebf81c06ebe75cc09a12cba27b3a
aaron.lee.garrett <aaron.lee.garrett@ae8af3b4-1321-11de-99c7-9d618077940f>
2009-12-02 06:20:56 +0000

    Added a function to calculate the hypervolume indicator in the emo module.

--- ecspy/emo.py
+++ ecspy/emo.py
@@ -17,6 +17,7 @@
        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import math
 from ecspy import ec
 from ecspy import archivers
 from ecspy import selectors
@@ -74,8 +75,82 @@ class Pareto(object):
         
     def __repr__(self):
         return str(self.values)
+
+
+def hypervolume(pareto_set, reference_point=None):
+    """Calculates the hypervolume by slicing objectives (HSO).
+    
+    This function calculates the hypervolume (or S-measure) of a nondominated
+    set using the Hypervolume by Slicing Objectives (HSO) procedure of While,
+    et. al. The *pareto_set* should be a list of lists of objective values.
+    The *reference_point* may be specified or it may be left as the default 
+    value of None. In that case, the reference point is calculated to be the
+    maximum value in the set for all objectives (the ideal point). As with 
+    all other functions, this function assumes that objectives are to be
+    maximized.
+    
+    Arguments:
+    
+    - *pareto_set* -- the list of objective values comprising the Pareto front
+    - *reference_point* -- the reference point to be used (default None)
+    
+    """
+    def dominates(p, q, k=None):
+        if k is None:
+            k = len(p)
+        d = True
+        while d and k < len(p):
+            d = not (q[k] > p[k])
+            k += 1
+        return d
         
-        
+    def insert(p, k, pl):
+        ql = []
+        while pl and pl[0][k] > p[k]:
+            ql.append(pl[0])
+            pl = pl[1:]
+        ql.append(p)
+        while pl:
+            if not dominates(p, pl[0], k):
+                ql.append(pl[0])
+            pl = pl[1:]
+        return ql
+
+    def slice(pl, k, ref):
+        p = pl[0]
+        pl = pl[1:]
+        ql = []
+        s = []
+        while pl:
+            ql = insert(p, k + 1, ql)
+            p_prime = pl[0]
+            s.append((math.fabs(p[k] - p_prime[k]), ql))
+            p = p_prime
+            pl = pl[1:]
+        ql = insert(p, k + 1, ql)
+        s.append((math.fabs(p[k] - ref[k]), ql))
+        return s
+
+    ps = pareto_set
+    ref = reference_point
+    n = min([len(p) for p in ps])
+    if ref is None:
+        ref = [max(ps, key=lambda x: x[o])[o] for o in xrange(n)]
+    pl = ps[:]
+    pl.sort(key=lambda x: x[0], reverse=True)
+    s = [(1, pl)]
+    for k in xrange(n - 1):
+        s_prime = []
+        for x, ql in s:
+            for x_prime, ql_prime in slice(ql, k, ref):
+                s_prime.append((x * x_prime, ql_prime))
+        s = s_prime
+    vol = 0
+    for x, ql in s:
+        vol = vol + x * math.fabs(ql[0][n - 1] - ref[n - 1])
+    return vol
+
+
 class NSGA2(ec.EvolutionaryComputation):
     """Evolutionary computation representing the nondominated sorting genetic algorithm.
     
