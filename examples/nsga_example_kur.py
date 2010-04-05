2b21ab0a753dfc9584423a8045b47b183f968002
jelleferinga <jelleferinga@ae8af3b4-1321-11de-99c7-9d618077940f>
2010-04-05 19:00:53 +0000

    Aaron, I was hoping you could have a look at the nsga_example_kur.py file. I don't see why its not working out; perhaps I've made a mistake translating the formula ( surely you have the NSGA paper ;') ... Making sure KUR converges nicely is something I'd love to see completed; SCH is converging nicely now, but seeing ecspy converge KUR would be a total BLAST! Somehow when I see a plot of this function strikes an -all is good- sensation with me...
    ( btw; the pybrain project has a nice version of this, if you'd appreciate a reference )

--- examples/nsga_example_kur.py
+++ examples/nsga_example_kur.py
@@ -1,11 +1,10 @@
-import random
-import time
+import random, time, math
+
 from ecspy import emo, ec
 from ecspy import selectors
 from ecspy import variators
 from ecspy import observers
 from ecspy import terminators
-
 from ecspy.contrib.utils import round_fitness
 
 def generate_candidate(random, args):
@@ -17,24 +16,41 @@ def generate_candidate(random, args):
         upper_bound = args['upper_bound']
     except KeyError:
         upper_bound = 1
-        
-    #return ec.Individual([random.random() * (upper_bound - lower_bound) + lower_bound], args['maximize'])
-    return [random.random() * (upper_bound - lower_bound) + lower_bound]
+    return [random.random() * (upper_bound - lower_bound) + lower_bound for i in range(2)]
 
 def evaluate_sch(candidates, args):
+    
+    def kur1(cs):
+        result = 0.
+        for i in range(len(cs)-1):
+            xi = math.pow(cs[i], 2.0) 
+            xj = math.pow(cs[i]+1., 2.0)
+            aux = -0.2 * math.sqrt((xi+xj))
+            result += -10. * math.exp(aux)
+        return result  
+
+    def kur2(cs):
+        result = 0.
+        for i in range(len(cs)):
+            xi = cs[i] 
+            result += math.pow(abs(xi), 0.8) + (5.0 * math.sin(math.pow(xi, 3.0)))
+        return result  
+    
     maximize = args['maximize']
     fitness = []
     for cs in candidates:
-        x = cs[0]**2
-        y = (cs[0]-2)**2 
+        x = kur1(cs)
+        y = kur2(cs) 
+#        x = cs[0]**2
+#        y = (cs[0]-2)**2 
         fitness.append(emo.Pareto(maximize, [x, y]))
     return fitness
 
-def converged_sch(population, num_generations, num_evaluations, args):
-    if num_generations < 20:
-        return False
-    pop = round_fitness(population, 1)
-    return [0.0, 4.0] in pop
+def converged_kur(population, num_generations, num_evaluations, args):
+    if num_generations == 20:
+        return True
+    #pop = round_fitness(population, 1)
+    #return [0.0, 4.0] in pop
 
 def my_observer(population, num_generations, num_evaluations, args):
     try:
@@ -69,11 +85,10 @@ def main(do_plot=True, prng=None):
                             generator=generate_candidate, 
                             evaluator=evaluate_sch, 
                             pop_size=100,
-                            terminator=[terminators.evaluation_termination,converged_sch], 
-                            max_evaluations=1e8,
-                            lower_bound=-1000.,
-                            upper_bound= 1000.,
-                            #mut_range=1.
+                            terminator=[terminators.evaluation_termination,converged_kur], 
+                            max_evaluations=1e6,
+                            lower_bound=-5.,
+                            upper_bound= 5.,
                             )
     
     print('*******************************')
@@ -83,12 +98,14 @@ def main(do_plot=True, prng=None):
         y = []
         for f in final_arc[1:]:
             print(f)
-            # filter out left over values in the archive...
-            a,b = abs(f.fitness[0]), abs(f.fitness[1])
-            if a>4 or b>4:
-                continue 
+#             filter out left over values in the archive...
+#            a,b = abs(f.fitness[0]), abs(f.fitness[1])
+#            if a>4 or b>4:
+#                continue 
+            
             x.append(f.fitness[0])
             y.append(f.fitness[1])
+            
         pylab.scatter(x, y, color='b')
         pylab.show()
         #pylab.savefig('nsga2-front.pdf', format='pdf')
