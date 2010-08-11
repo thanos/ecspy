844fa7bd036ea16a96d44cbd6efae0af6e52cd77
hrudstrom <hrudstrom@ae8af3b4-1321-11de-99c7-9d618077940f>
2010-08-11 13:30:20 +0000

    Cartesian GP working, added comments cgp_regression wont converge with the example regression problems specified in the paper :( still there is no crossover operator defined... hopefully that could help

--- ecspy/contrib/cartesian_gp.py
+++ ecspy/contrib/cartesian_gp.py
@@ -1,14 +1,25 @@
+from docutils.parsers.rst.directives import encoding
+
 __author__ = 'henrik'
-
-#from ecspy.contrib.variator_pipe import iterate, iterate, composite_variator, composite_variator, gaussian_mutation, bound
-
 
 import networkx, random, math, unittest, time
 from itertools import izip_longest, dropwhile
 
 class CartesianGraphEncoding:
+    '''
+       Stores the specification of the gp array to generate variation operators and
+       decode the candidate matrix into a graph
+    '''
     def __init__(self, n_inputs, matrix_size, node_size, n_outputs, levelsback):
-        '''matrix in the format [cols] x [rows] x [[node_size[0]][node_size[1]]]'''
+        '''
+        arguments:
+
+        - *n_inputs* -- number of rows in the input column (including constants)
+        - *matrix_size* -- tuple (columns, rows), excluding input and output columns
+        - *node_size* -- tuple (number of edges pr node, number of data fields pr node)
+        - *n_outputs* -- number of rows in the output column
+        - *levelsback* -- parameter that limits how many columns back a node can connect to, minimum 1
+        '''
         self.n_inputs = n_inputs
         self.matrix_size = matrix_size
         self.node_size = node_size
@@ -16,12 +27,22 @@ class CartesianGraphEncoding:
         self.levelsback = levelsback
 
     def generator(self, random_edge, random_data):
+        '''
+        returns a generator function that generates a candidate matrix in the form:
+        cols x rows x [edges, data]
+
+        Arguments:
+
+        - *random_edge* -- function to generate a random float edge index between 0. 1.
+        - *random_data* -- function to generate a random data value (problem dependent)
+        '''
         def gen(random, args):
             matrix = []
             for col in xrange(self.matrix_size[0]):
                 col_rows = []
                 for row in xrange(self.matrix_size[1]):
                     edges = [random_edge() for _ in xrange(self.node_size[0])]
+                    #TODO: should perhaps return the whole data array, to allow multiple data types etc
                     data = [random_data() for _ in xrange(self.node_size[1])]
                     col_rows.append([edges, data])
                 matrix.append(col_rows)
@@ -30,6 +51,12 @@ class CartesianGraphEncoding:
         return gen
 
     def mutator(self, rate, mutate_edge, mutate_data):
+        '''
+        Arguments:
+
+        - *mutate_edge* -- function to mutate a float edge index between 0. 1.
+        - *mutate_data* -- function to mutate a random data value (problem dependent)
+        '''
         def mut(random, cand, args):
             maybe_mutate = lambda v, func: func(v) if random.random() < rate else v
             matrix = []
@@ -45,51 +72,96 @@ class CartesianGraphEncoding:
         return lambda random, candidates, args: [mut(random, candidate, args) for candidate in candidates]
 
     def decode(self, matrix, graph=None):
+        '''
+        creates a networkx.MultiDiGraph from the given candidate matrix
+        '''
+
+        #multiple cgps could potentially be stacked to implement rudimentary typing (not tested)
         graph = graph or networkx.MultiDiGraph()
-        matrix  = [[([1], [1])]] * self.n_inputs + matrix
+
+        #the input column dont encode any data, so the input column
+        #has to be appended with empty node values
+        matrix  = [[[[], []]] * self.n_inputs] + matrix
+
         for col, rows in enumerate(matrix):
             for row, (inputs, data) in enumerate(rows):
+                #node identifier in the graph
                 node = (col, row)
-                node_data = dict(('s'+str(i), v) for i, v in enumerate(data))
+
+                #create a dictionary of the data fields
+                node_data = dict((i, v) for i, v in enumerate(data))
+
+                #set input and output flags for first and last column
                 if col == 0:
                     node_data['input'] = True
                 if col == self.matrix_size[0] + 1:
                     node_data['output'] = True
-                graph.add_node(node, **node_data)
-                if inputs is None:
-                    import ipdb; ipdb.set_trace()
-                decode = lambda input: self._decode_edge(col, input)
+                graph.add_node(node, node_data)
 
+                #get edges as a tuple of node tuples (col, row)
+                decode = lambda input: self._decode_input_node(col, input)
                 edges = [(decode(input), node) for input in inputs]
+
                 graph.add_edges_from(edges)
         return graph
 
-    def _decode_edge(self, col,  float_index):
+    def _decode_input_node(self, col,  float_index):
+        '''
+            returns the source node for an input edge (col, row) tuple
+            - *col* -- the column of the destination node
+            - *float_index* -- encoded value of edge
+        '''
+        #determine column range
         mincol = max(0, (col - self.levelsback))
         range = col - mincol
         if range == 0:
             return None
+        #scale float index to range
         float_index = mincol + (float_index % 1.) * range
+
+        #column is floored value
         col = int(float_index)
+
+        #determine number of rows
         row_len = self.matrix_size[1]
         if col == 0:
             row_len = self.n_inputs
+        if col == self.matrix_size[0] + 1:
+            row_len = self.n_outputs
+
+        #scale remainder of float_index to row range
         row = int(row_len * (float_index - col))
+
         return col, row
 
 def node_function_template(node, args, graph):
     return 0.0
 
 class CallableGraph:
-    def __init__(self, graph, function, output_function=None, constants=None):
+    '''
+       A function defined by an acyclic graph with function mapped to the nodes
+    '''
+
+
+    def __init__(self, graph, function):
+        '''
+           Arguments:
+
+           - *graph* -- networkx.DiGraph or MultiDiGraph
+           - *function* -- function to be executed at each node.
+           signature: (node, args_values, node_data)
+        '''
         self.graph = graph
         self.function = function
-        self.output_function = output_function or self.output_function
         self.values = None
 
         self.inputs = filter(lambda n: 'input' in graph.node[n], graph.nodes())
         self.outputs = filter(lambda n: 'output' in graph.node[n], graph.nodes())
 
+
+        #call order array is an list of all nodes with corresponding input nodes ordered
+        #by their dependency.
+        #TODO: better would probably be to generate the function code and compile/eval it
         call_order = []
         for out_node in self.outputs:
             path = networkx.dfs_postorder(graph.reverse(), out_node)
@@ -97,27 +169,37 @@ class CallableGraph:
 
         arg_func = lambda node: (e[0] for e in graph.in_edges_iter(node))
         self.call_order = tuple((node, list(arg_func(node))) for node in call_order)
-        #self.constants = constants or []
 
     def __call__(self, input_values):
+        '''
+        call the function.
+        Arguments:
+
+        - *input_values* -- array of input values (including constants)
+
+        '''
         values = {}
         output_values = []
-        #input_values = input_values + self.constants
+
+        #iterate over node, preceding nodes
         for node, arg_nodes in self.call_order:
+            #map the input values to the input nodes in the values dict
             if node[0] == 0:
                 values[node] = input_values[node[1]]
                 continue
-            args = list((values[arg] for arg in arg_nodes))
-            if node in self.outputs:
-                values[node] = self.output_function(node, args, self.graph.node[node])
-                output_values.append(values[node])
-            else:
-                values[node] = self.function(node, args, self.graph.node[node])
-        self.values = values
-        return output_values
 
-    def output_function(self, node, args, data):
-        return args
+            #fetch argument values (call order guarantees that input nodes are already in the dict
+            args = list((values[arg] for arg in arg_nodes))
+
+            #call node function
+
+
+            self.values = values
+            if not node in self.outputs:
+                values[node] = self.function(node, args, self.graph.node[node])
+            else:
+                output_values.append(args[0])
+        return output_values
 
 def draw_graph(graph, scale = 20):
 
@@ -131,4 +213,56 @@ def draw_graph(graph, scale = 20):
     networkx.draw(graph, pos)
 
 class CGPTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        matrix_size = 4, 3
+        n_inputs, n_outputs = 2, 1
+        node_size = 2, 1
+        levelsback = 3
+        self.encoding = CartesianGraphEncoding(n_inputs, matrix_size, node_size, n_outputs, levelsback)
+
+
+    def test_decode_edge(self):
+        def assert_decode(node, col, val):
+            dnode = enc._decode_input_node(col, val)
+            assert node == dnode, ("decoding failed %s != %s" % (node, dnode)) 
+        enc = self.encoding
+        cols, rows = enc.matrix_size
+        assert_decode((0,0), 1, 0.0)
+        assert_decode((0,1),1, 0.5)
+        assert_decode((1,0),2, 0.5)
+        assert_decode((1,2),2, 0.9999)
+        assert_decode((0,1),3, 0.17)
+        assert_decode((1,2),3, 1.0 / 3 + 2. / 9)
+        assert_decode((2,0),5, 0.0)
+        assert_decode((3,0),5, 1.0 / 3)
+        assert_decode((4,2),5, 0.9999)
+        
+    def test_generator(self):
+        enc = self.encoding
+        gen = enc.generator(random_data=lambda: 'a', random_edge=lambda: 1.0)
+        matrix = gen(random, {})
+        assert len(matrix) == enc.matrix_size[0] + 1 #output column encoded, input column not
+        assert len(matrix[0]) == enc.matrix_size[1]
+        assert len(matrix[0][0]) == 2 #edge list and data list
+        assert len(matrix[0][0][0]) == enc.node_size[0] #edge list and data list
+        assert len(matrix[0][0][1]) == enc.node_size[1] #edge list and data list
+        assert len(matrix[-1]) == enc.n_outputs
+        assert len(matrix[-1][0][0]) == 1 #one input
+        assert len(matrix[-1][0][1]) == 0 #no data
+
+    def test_graph_decoder(self):
+        enc = self.encoding
+        gen = enc.generator(random_data=lambda: 'a', random_edge=random.random)
+        matrix = gen(random, {})
+        graph = enc.decode(matrix)
+        n_nodes = enc.matrix_size[0] * enc.matrix_size[1]
+        print n_nodes, len(graph.nodes()), len(graph.edges())
+
+        #assert created
+        assert len(graph.nodes()) == n_nodes + enc.n_outputs + enc.n_inputs
+        assert len(graph.edges()) == n_nodes * 2 + enc.n_outputs
+
+        #assert that all edges are valid, ie not on same column and within levelsback
+        for (c1, r1), (c2, r2) in graph.edges():
+            assert c2 - c1 > 0
+            assert c2 - c1 <= enc.levelsback
--- examples/cgp_regression.py
+++ examples/cgp_regression.py
@@ -1,14 +1,19 @@
-
-
-from ecspy.contrib.cartesian_gp import CartesianGraphEncoding, CallableGraph
-
-__author__ = 'henrik'
-
 '''
-Created on Jul 22, 2010
+Created on Jul 26, 2010
 
 @author: henrik
+
+symbolic regression of a 6th order polynominal
+as described in original Cartesian genetic programming paper: Miller 2008 (GECCO Proceedings)
+
 '''
+__author__ = 'henrik'
+
+from ecspy.contrib.cartesian_gp import CartesianGraphEncoding, CallableGraph
+from sympy.core.symbol import Symbol
+from sympy.core.numbers import RealNumber
+
+
 from random import Random
 import random
 from time import time
@@ -16,95 +21,127 @@ from ecspy import ec, observers, terminators, variators
 import math
 from itertools import *
 from ecspy.selectors import truncation_selection, tournament_selection
-matrix_size = (4, 3)
-node_size = (2, 1)
-n_inputs, n_outputs = 1, 1
-n_args, output_args = 2, 1
-levelsback = 2
-encoding = CartesianGraphEncoding(n_inputs, matrix_size, node_size, n_outputs, levelsback)
 
+
+def div(num, den):
+    '''
+    return numerator if denominator is 0
+    '''
+    return num if den == 0.0 else num / den
+
+
+ops = [float.__add__, float.__sub__, float.__mul__, div]
 
 def arithmetic(node, args, data):
     a, b = args
     op = data[0]
-    op = int((op % 1) * 4)
-    ops = [float.__add__, float.__sub__, float.__mul__, lambda a, b: a / (b or 1e-6)]
+    op = int((op % 1) * len(ops))
     val = ops[op](a,b)
     return val
 
-target_function = lambda x: x + 1 - x*x
 
+def sympy_arithmetic(node, args, data):
+    a, b = list(args)
+    op = int((data[0] % 1) * 4)
+    ops = [a.__add__, a.__sub__, a.__mul__, a.__div__]
+    val = ops[op](b)
+    return val
 
+def get_fitness_function(target_function, constants):
+    '''
 
-def fitness(candidates, args):
-    fitness = []
-    for cand in candidates:
-        fit = 0.0
+    The fitness cases were 50 random values of X from the interval [-1.0, +1.0].
+    These were fixed throughout the evolutionary run. The fitness of a program was
+    defined as the sum over the 50 fitness cases of the sum of the absolute value of
+    the error between the value returned by the program and that corresponding to the
+    sixth order polynomial.
+    --Miller
+    '''
+    def fitness(candidates, args):
+        fitness = []
+        sample_points = [random.random()*2-1 for _ in xrange(50)]
+        for cand in candidates:
+            fit = 0.0
+            evolved_function = CallableGraph(encoding.decode(cand), arithmetic)
 
-        evolved_function = CallableGraph(encoding.decode(cand), arithmetic)
-        #sympy_exp = evolved_function([Symbol('x')])[0][0]
-        diffs = [None] * 10
-        prev_t = None
-        prev_s = None
-        for s in xrange(-10, 10):
-            target = target_function(float(s))
-            s = s / 5.
-            #solution = sympy_exp.subs({'x':s})
-            solution = evolved_function([s])[0][0]
-            #if solution.__class__ in [NaN, NegativeInfinity, Infinity]:
-            #    solution = 1e10
-
-
-            fit += math.sqrt(abs(target - solution))
-        fitness.append(fit)
+            for x in sample_points:
+                x = random.random()*2 - 1
+                target = target_function(x)
+                solution = evolved_function([x]+constants)[0]
+                fit += math.sqrt(abs(target - solution))
+            fitness.append(fit)
+        return fitness
     return fitness
-
 def mutate_edge(v):
-    return random.random()
+    return (v + random.random() * 0.4-0.2) % 1.0
 
 def mutate_data(v):
-    return random.randint(0,4)
+    return (v + random.randint(-1,1)) % 4
 
 def generate_data():
     return random.randint(0,4)
 
+
+
 if __name__ == '__main__':
 
-    prng = Random()
-    prng.seed(time())
+    #The population size chosen was 10. The number of generations was equal to 8000.
+    #The crossover rate was 100% (entire population replaced by children). The mutation rate was 2%,
+    #i.e. on average 2% of the all the genes in the population were mutated. Other parameters were
+    #as follows: nr =1, nc =10, l =10.
+
+
+
+    constants = [1.]
+
+
+    matrix_size = 10, 1
+    n_inputs, n_outputs = 2, 1
+    node_size = 2, 1
+    levelsback = 10
+    encoding = CartesianGraphEncoding(n_inputs, matrix_size, node_size, n_outputs, levelsback)
+
+    target = lambda x: x ** 6 - 2 * x ** 4 + x ** 2
+    fitness_function = get_fitness_function(target, constants)
+
 
     mutator = encoding.mutator(0.1, mutate_edge=mutate_edge, mutate_data=mutate_data)
     generator = encoding.generator(random_edge=random.random, random_data=generate_data)
+
+
+    prng = Random()
+    prng.seed(time())    
     ga = ec.GA(prng)
 
-    ga.observer = [observers.screen_observer, observers.file_observer]
+    ga.observer = [observers.mini_screen_observer, observers.file_observer]
     ga.terminator = terminators.evaluation_termination
     ga.variator = [mutator]
     ga.selector = tournament_selection
     #ga.bounder = wrap_around(0., 1.)
     start = time()
     print "setup"
-    final_pop = ga.evolve(evaluator=fitness,
+    final_pop = ga.evolve(evaluator=fitness_function,
                           generator=generator,
-                          max_evaluations=10000,
+                          max_evaluations=120000,
                           num_elites=1,
-                          pop_size=200,
+                          pop_size=12,
                           mean=0.0, stdev=0.1,
                           mutation_rate=0.02,
                           tourn_size=4,
                           num_generations=1,
                           maximize=False)
     stop = time()
-    stat_file.close()
-    ind_file.close()
+    #stat_file.close()
+    #ind_file.close()
 
     print('***********************************')
     print('Total Execution Time: %0.5f seconds' % (stop - start))
-    for ind in final_pop:
-        print(str(ind))
+    #for ind in final_pop:
+    #    print(str(ind))
 
     exps = []
     final_pop.sort(key=lambda c: c.fitness)
-
-    #final_exps = [(create_function(c.candidate, decoder, sympy_arithmetic)([Symbol('x')]), c.fitness) for c in final_pop]
-    #print final_exps
+    item = lambda c: [CallableGraph(encoding.decode(c.candidate), sympy_arithmetic)([Symbol('x'), RealNumber(1.)])]
+    final_exps = [(c.fitness, item(c)[0][0]) for c in final_pop]
+    for fit, exp in final_exps:
+        print fit, exp
